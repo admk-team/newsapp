1b143741f699996de284bc0034518f7742d8d953
Khizar wahab <khizarwahab19@gmail.com>
2022-12-14 13:19:44 +0500

    toggle switch

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,26 @@ $('.dropdown-menu.new').on("click", function (e) {
     e.stopPropagation();
     }
 });
+
+$(document).ready(function () {
+    $('.toggle-switch-new').click(function () {
+        let active = $(this).find('label.active');
+        let inactive = $(this).find('label:not(.active)');
+        let input = $(this).find('input[type="hidden"]');
+        active.removeClass('active');
+        inactive.addClass('active');
+        input.val(inactive.data('value'));
+
+        if ("createEvent" in document) {
+            var evt = document.createEvent("HTMLEvents");
+            evt.initEvent("change", false, true);
+            input[0].dispatchEvent(evt);
+        }
+        else
+            input[0].fireEvent("onchange");
+    });
+})
+
+function test(self) {
+    console.log(self.value);
+}
